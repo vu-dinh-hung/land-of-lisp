1d23f6e9e2dc0d2571f7c7e72e75d190fe230825
Hung Vu <vudinhhung942k@gmail.com>
2025-11-15 06:33:20 -0500

    Chapter 5

--- wizard_adventure.lisp
+++ wizard_adventure.lisp
@@ -40,3 +40,22 @@
     (describe-location *location* *nodes*)
     (describe-paths *location* *edges*)
     (describe-objects *location* *objects* *object-locations*)))
+
+(defun walk (direction)
+  (let ((next (find direction
+                  (cdr (assoc *location* *edges*))
+                :key #'cadr)))
+    (if next
+        (progn (setf *location* (car next))
+               (look))
+        '(you cannot go that way.))))
+
+(defun pickup (object)
+  (cond ((member object
+                 (objects-at *location* *objects* *object-locations*))
+          (push (list object 'body) *object-locations*)
+          `(you are now carrying the ,object))
+        (t '(you cannot get that.))))
+
+(defun inventory ()
+  (cons 'items- (objects-at 'body *objects* *object-locations*)))
