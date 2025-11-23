4b3ea00eb24d9a8ce374a90da41d88be55003ba0
Hung Vu <vudinhhung942k@gmail.com>
2025-11-23 05:53:12 -0500

    Formatting

--- wizard_adventure.lisp
+++ wizard_adventure.lisp
@@ -1,6 +1,9 @@
-(defparameter *nodes* '((living-room (you are in the living-room. a wizard is snoring loudly on the couch.))
-                        (garden (you are in a beautiful garden. there is a well in front of you.))
-                        (attic (you are in the attic. there is a giant welding torch in the corner.))))
+(defparameter *nodes* '((living-room (you are in the living-room.
+                                    a wizard is snoring loudly on the couch.))
+                        (garden (you are in a beautiful garden.
+                                     there is a well in front of you.))
+                        (attic (you are in the attic.
+                                    there is a giant welding torch in the corner.))))
 
 (defparameter *edges* '((living-room (garden west door)
                                      (attic upstairs ladder))
@@ -26,36 +29,16 @@
   (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
 
 (defun objects-at (loc objs obj-locs)
-  (labels ((at-loc-p (obj) (eq loc (cadr (assoc obj obj-locs)))))
+  (labels ((at-loc-p (obj)
+             (eq loc (cadr (assoc obj obj-locs)))))
     (remove-if-not #'at-loc-p objs)))
 
 (defun describe-objects (loc objs obj-locs)
-  (labels ((describe-obj (obj) `(you see a ,obj on the floor.)))
-    (apply #'append
-      (mapcar #'describe-obj
-        (objects-at loc objs obj-locs)))))
+  (labels ((describe-obj (obj)
+             `(you see a ,obj on the floor.)))
+    (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-locs)))))
 
 (defun look ()
-  (append
-    (describe-location *location* *nodes*)
-    (describe-paths *location* *edges*)
-    (describe-objects *location* *objects* *object-locations*)))
-
-(defun walk (direction)
-  (let ((next (find direction
-                  (cdr (assoc *location* *edges*))
-                :key #'cadr)))
-    (if next
-        (progn (setf *location* (car next))
-               (look))
-        '(you cannot go that way.))))
-
-(defun pickup (object)
-  (cond ((member object
-                 (objects-at *location* *objects* *object-locations*))
-          (push (list object 'body) *object-locations*)
-          `(you are now carrying the ,object))
-        (t '(you cannot get that.))))
-
-(defun inventory ()
-  (cons 'items- (objects-at 'body *objects* *object-locations*)))
+  (append (describe-location *location* *nodes*)
+          (describe-paths *location* *edges*)
+          (describe-objects *location* *objects* *object-locations*)))
